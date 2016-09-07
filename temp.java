ecb6bc5fb292aa9171bd8e304d7eaf6a0c5923ce
aswhadhaaanbukkaman <aswhadhaa111@gmail.com>
2016-09-07 11:26:10 +0530

    Create temp.java

---
+++ temp.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class temp{
+public int lengthOfLongestSubstringTwoDistinct(String s) {
+    int max=0;
+    HashMap<Character,Integer> map = new HashMap<Character, Integer>();
+    int start=0;
+ 
+    for(int i=0; i<s.length(); i++){
+        char c = s.charAt(i);
+        if(map.containsKey(c)){
+            map.put(c, map.get(c)+1);
+        }else{
+            map.put(c,1);
+        }
+ 
+        if(map.size()>2){
+            max = Math.max(max, i-start);
+ 
+            while(map.size()>2){
+                char t = s.charAt(start);
+                int count = map.get(t);
+                if(count>1){
+                    map.put(t, count-1);
+                }else{
+                    map.remove(t);
+                }
+                start++;
+            }
+        }
+    }
+ 
+    max = Math.max(max, s.length()-start);
+ 
+    return max;
+    public static void main(String str[])
+    {
+    Scanner sc=new Scanner(System.in);
+    String s=sc.nextLine();
+    temp p1=new temp();
+    int p=p1.lengthOfLongestSubstringTwoDistinct(String s)
+    System.out.println(p);
+    
+}
